32964de52bc85011fff40b5029ebeb96306ffada
hasover <hasover@gmail.com>
2021-07-26 21:49:56 +0300

    4.1. JSON формат. [#238554]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,21 +1,45 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.job4j.dream.model.Email;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GreetingServlet extends HttpServlet {
+    private final List<Email> emails = new CopyOnWriteArrayList<>();
+    private final static Gson GSON = new GsonBuilder().create();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Email email = GSON.fromJson(req.getReader(), Email.class);
+        emails.add(email);
+
+        resp.setContentType("application/json; charset=utf-8");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(email);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        PrintWriter out = new PrintWriter(resp.getOutputStream(), true, StandardCharsets.UTF_8);
-        out.println("nice to meet you," + name);
+        OutputStream out = resp.getOutputStream();
+        String json = GSON.toJson(emails);
+        out.write(json.getBytes(StandardCharsets.UTF_8));
         out.flush();
+        out.close();
     }
 }
